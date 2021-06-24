c70482297a98c6b216eec43d26dda1407f7dd000
Yann Lamidon <yann.lamidon@gmail.com>
2021-06-24 07:53:10 +0100

    Lecture 5 add gorountine examples

---
+++ Lecture5/goroutine/sleep-cancel.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"sync"
+	"time"
+)
+
+var done bool
+var mu sync.Mutex
+
+func main() {
+	time.Sleep(1 * time.Second)
+	println("started")
+	go periodic()
+	time.Sleep(5 * time.Second) // wait for a while so we can observe what a ticker does
+	mu.Lock()
+	done = true
+	mu.Unlock()
+	println("cancelled")
+	time.Sleep(3 * time.Second) // observe no output
+}
+
+func periodic() {
+	for {
+		println("tick")
+		time.Sleep(1 * time.Second)
+		mu.Lock()
+		if done {
+			mu.Unlock()
+			return
+		}
+		mu.Unlock()
+	}
+}
